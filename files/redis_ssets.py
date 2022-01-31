844bdd7a90527904e73613d7448a86648a2a9e58
abhi <abhishekprakash256@gmail.com>
2022-01-30 22:04:02 -0500

    sorted-sets

--- files/redis_ssets.py
+++ files/redis_ssets.py
@@ -3,15 +3,16 @@ the file has the code for redis sorted sets
 """
 
 
-#the code is not able to work for adding the elements
-
 import redis
 
 c = redis.Redis(host = 'localhost' , port = 6379)
 
 #adding the items
 
-data = "hello world"
-score = 1 
-c.zadd("redis_key_name", data, score)
+data = {'key_0' : 0 , 'key_1': 1}
 
+c.zadd("redis_key_name", data)
+
+
+#getting the elements
+print(c.zrange('redis_key_name', 0 , -1, withscores = 'withscores'))
